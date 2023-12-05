9ae8b6abb01b80f68a4963a24c03d29c432afaf8
kkumyk <k.kumykova@gmail.com>
2023-12-05 18:05:05 +0000

    display ingredients count on the recipe card

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -4,20 +4,13 @@ interface Props {
     recipe: Recipe
 }
 
-
 // create RecipeCard React component
 const RecipeCard = ({recipe}: Props) => {
-
     return (
-
         <div className="recipe-card">
-
             <img src={recipe.image}></img>
-
-            <div className="recipe-card-title">
-                <h3>{recipe.title}</h3>
-            </div>
-
+            <div className="recipe-card-title"><h3>{recipe.title}</h3></div>
+            <div className="ingredientsCount"><h4>{recipe.usedIngredientCount}</h4></div>
         </div>
     );
 };
