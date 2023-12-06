9f969c1eb4f82dcf1468a6fecd5a3747e7f371fa
kkumyk <k.kumykova@gmail.com>
2023-12-06 11:53:33 +0000

    add getRecipeSummary to call summary endpoint

--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -11,13 +11,12 @@ export const searchRecipes = async (searchTerms: string, page: number, ranking: 
     const queryParams = {
         apiKey: apiKey,
         ingredients: searchTerms,
-        number: "10",
+        number: "99",
         ranking: "1",
-        sort: "max-used-ingredients"
+        sort: "max-used-ingredients",
     }
     // attach query params to URL
     url.search = new URLSearchParams(queryParams).toString();
-
     try {
         const searchResponse = await fetch(url);
         const resultsJson = await searchResponse.json();
@@ -26,3 +25,22 @@ export const searchRecipes = async (searchTerms: string, page: number, ranking: 
         console.log(error);
     }
 };
+
+export const getRecipeSummary = async (recipeId: string) => {
+    if (!apiKey) {
+      throw new Error("API Key not found");
+    }
+  
+    const url = new URL(
+      `https://api.spoonacular.com/recipes/${recipeId}/summary`
+    );
+    const params = {
+      apiKey: apiKey,
+    };
+    url.search = new URLSearchParams(params).toString();
+  
+    const response = await fetch(url);
+    const json = await response.json();
+  
+    return json;
+  };
