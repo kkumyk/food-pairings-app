315160d2b3b1f238f75a7ea677cb1fdf3d365cd6
kkumyk <k.kumykova@gmail.com>
2023-12-13 13:15:20 +0000

    comment out favs related code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,9 @@ for this to happen, import the contents of the recipe-api.ts file
 // this line creates a new express app;
 const app = express();
 
+// http://localhost:5000/api/recipes/search?searchTerms=beef,+tomatoes&page=1&ranking=1&sort=max-used-ingredients&number=1
+// http://localhost:5000/api/recipes/646572/ingredients
+
 const prismaClient = new PrismaClient();
 
 /*
@@ -32,58 +35,65 @@ app.get("/api/recipes/search", async (req, res) => {
     return res.json(results);
 })
 
-app.get("/api/recipes/:recipeId/summary", async (req, res) => {
+app.get("/api/recipes/:recipeId/ingredients", async (req, res) => {
     const recipeId = req.params.recipeId;
-    const results = await RecipeAPI.getRecipeSummary(recipeId);
+    const results = await RecipeAPI.getRecipeIngredients(recipeId);
     return res.json(results);
 });
 
-app.post("/api/recipes/favourite", async (req, res) => {
-    const recipeId = req.body.recipeId;
 
-    try {
-        const favouriteRecipe = await prismaClient.favouriteRecipes.create({
-            data: {
-                recipeId: recipeId, // take recipe id from the endpoint
-            },
-        });
-        return res.status(201).json(favouriteRecipe);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: "Oops, something went wrong" });
-    }
-});
+// app.get("/api/recipes/:recipeId/summary", async (req, res) => {
+//     const recipeId = req.params.recipeId;
+//     const results = await RecipeAPI.getRecipeSummary(recipeId);
+//     return res.json(results);
+// });
+
+// app.post("/api/recipes/favourite", async (req, res) => {
+//     const recipeId = req.body.recipeId;
+
+//     try {
+//         const favouriteRecipe = await prismaClient.favouriteRecipes.create({
+//             data: {
+//                 recipeId: recipeId, // take recipe id from the endpoint
+//             },
+//         });
+//         return res.status(201).json(favouriteRecipe);
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ error: "Oops, something went wrong" });
+//     }
+// });
+
+// app.get("/api/recipes/favourite", async (req, res) => {
+//     try {
+//         const recipes = await prismaClient.favouriteRecipes.findMany();
+//         const recipeIds = recipes.map((recipe) => recipe.recipeId.toString());
+
+//         const favourites = await RecipeAPI.getFavouriteRecipesByIDs(recipeIds);
+
+//         return res.json(favourites);
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ error: "Oops, something went wrong" });
+//     }
+// });
 
 
-app.get("/api/recipes/favourite", async (req, res) => {
-    try {
-        const recipes = await prismaClient.favouriteRecipes.findMany();
-        const recipeIds = recipes.map((recipe) => recipe.recipeId.toString());
+// app.delete("/api/recipes/favourite", async (req, res) => {
+//     const recipeId = req.body.recipeId;
 
-        const favourites = await RecipeAPI.getFavouriteRecipesByIDs(recipeIds);
-
-        return res.json(favourites);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: "Oops, something went wrong" });
-    }
-});
-
-app.delete("/api/recipes/favourite", async (req, res) => {
-    const recipeId = req.body.recipeId;
-
-    try {
-        await prismaClient.favouriteRecipes.delete({
-            where: {
-                recipeId: recipeId,
-            },
-        });
-        return res.status(204).send();
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: "Oops, something went wrong" });
-    }
-});
+//     try {
+//         await prismaClient.favouriteRecipes.delete({
+//             where: {
+//                 recipeId: recipeId,
+//             },
+//         });
+//         return res.status(204).send();
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ error: "Oops, something went wrong" });
+//     }
+// });
 
 /*
 - a function to start the app
